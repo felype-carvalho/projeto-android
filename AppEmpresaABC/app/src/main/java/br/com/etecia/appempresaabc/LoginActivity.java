af66e5cb6b430fc51f80e8d066182c803ecac79b
LABORATORIO_INFO <felype.oliveira@etec.sp.gov.br>
2019-03-27 21:40:44 -0300

    Botões de transição de activity estão funcionado no AppEmpresaABC.

--- AppEmpresaABC/app/src/main/java/br/com/etecia/appempresaabc/LoginActivity.java
+++ AppEmpresaABC/app/src/main/java/br/com/etecia/appempresaabc/LoginActivity.java
@@ -1,14 +1,19 @@
 package br.com.etecia.appempresaabc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText txtUser, txtPassword;
+    Button sairSistema, entrarSistema;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +22,37 @@ public class LoginActivity extends AppCompatActivity {
 
         txtUser = (EditText)findViewById(R.id.edtUser);
         txtPassword = (EditText)findViewById(R.id.edtPassword);
+        entrarSistema = (Button)findViewById(R.id.btnSignIn);
+        sairSistema = (Button)findViewById(R.id.btnSignOut);
+
+        entrarSistema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginApp();
+            }
+        });
+
+        sairSistema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SairSistema();
+            }
+        });
     }
 
-    public void closeApp(View view) {
+    public void SairSistema(){
         finish();
     }
 
-    public void loginApp(View view) {
+
+    public void loginApp() {
         String user = txtUser.getText().toString();
         String password = txtPassword.getText().toString();
         if (user.equals("etecia")&& password.equals("etecia")) {
             Toast.makeText(getApplicationContext(),"Welcome!!!",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MenuPrincipalActivity.class);
+            startActivity(intent);
+
         } else {
             Toast.makeText(getApplicationContext(),"Connection failure, check your username or password.",Toast.LENGTH_SHORT).show();
         }
--- AppEmpresaABC/app/src/main/java/br/com/etecia/appempresaabc/MenuPrincipalActivity.java
+++ AppEmpresaABC/app/src/main/java/br/com/etecia/appempresaabc/MenuPrincipalActivity.java
@@ -1,13 +1,34 @@
 package br.com.etecia.appempresaabc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menuprincipal_layout);
+
+        back = (Button)findViewById(R.id.btnVoltar);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voltarLogin();
+            }
+        });
+    }
+
+    public void voltarLogin(){
+        Intent intent = new Intent(MenuPrincipalActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
+
+
