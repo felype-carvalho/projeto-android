8407bc48f713f47f386153b788358bf02598f880
felype <felype.oliveira@etec.sp.gov.br>
2019-04-03 22:11:08 -0300

    Desenvolvi o projeto Bandio

--- appBandio/app/src/main/java/br/com/etecia/appbandio/CadastroActivity.java
+++ appBandio/app/src/main/java/br/com/etecia/appbandio/CadastroActivity.java
@@ -9,5 +9,6 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_layout);
+
     }
 }
