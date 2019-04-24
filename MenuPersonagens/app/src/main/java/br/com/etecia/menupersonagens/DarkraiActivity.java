919d028d87bc476dfd49c2d33aab0b7d2b8f83e9
felype <felype.oliveira@etec.sp.gov.br>
2019-04-24 19:39:04 -0300

    Coloquei o nome dos Pokémons no Menu Personagens

---
+++ MenuPersonagens/app/src/main/java/br/com/etecia/menupersonagens/DarkraiActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.menupersonagens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DarkraiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.darkrai_layout);
+    }
+}
