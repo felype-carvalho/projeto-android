0cd4d4214891ae9cbb17563db9be9a7dc65e3367
felype <felype.oliveira@etec.sp.gov.br>
2019-04-24 20:21:41 -0300

    Crie um modelo de layout para os personagens]

---
+++ MenuPersonagens/app/src/main/java/br/com/etecia/menupersonagens/LatiosActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.menupersonagens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LatiosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.latios_layout);
+    }
+}
