fa292f7b157d79b70f3af02a76e592a0492e5b71
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-05-08 22:10:16 -0300

    deu tudo errado :(

--- AppMenuTwo/app/src/main/java/br/com/etecia/appmenutwo/MainActivity.java
+++ AppMenuTwo/app/src/main/java/br/com/etecia/appmenutwo/MainActivity.java
@@ -1,12 +1,12 @@
-package br.com.etecia.menuapp;
+package br.com.etecia.appmenutwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.idToolbar);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar= findViewById(R.id.idToolbar);
 
         getSupportActionBar().setTitle("AAA");
         getSupportActionBar().setIcon(R.drawable.ic_arrow_back_dp);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -30,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onContextItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.mShare:
                 Toast.makeText(getApplicationContext(), "Shared", Toast.LENGTH_SHORT).show();
@@ -51,9 +49,10 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
                 break;
 
-
         }
 
         return super.onContextItemSelected(item);
     }
 }
+
+
